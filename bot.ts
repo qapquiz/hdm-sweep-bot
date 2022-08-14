6384ac4961699bd4ae05522ead08fe05a02e530a
armariya <arm.armariya@gmail.com>
2022-08-14 16:46:26 +0700

    feat: add env and some function

--- bot.ts
+++ bot.ts
@@ -1,4 +1,11 @@
+import { BN } from '@project-serum/anchor';
 import { clusterApiUrl, Connection, Keypair, PublicKey } from '@solana/web3.js';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+const { DESIRE_PRICE, COLLECTION_PUBKEY_STR } = process.env;
+// const COLLECTION_PUBKEY = new PublicKey(COLLECTION_PUBKEY_STR!);
 
 const connection: Connection = new Connection(
 	clusterApiUrl('mainnet-beta'),
@@ -10,12 +17,29 @@ const marketPlaces = [
 	Keypair.generate().publicKey, // MagicEden
 ];
 
+function isBelowOrEqualDesirePrice(
+	marketPrice: number,
+	desirePrice: number
+): boolean {
+	return marketPrice <= desirePrice;
+}
+
+async function runBot(
+	connection: Connection,
+	marketPlaces: Array<PublicKey>,
+	interval: number
+) {
+	setInterval(async () => {
+		console.log('RUN BOT HERE');
+	}, interval);
+}
+
 async function start(
 	connection: Connection,
 	marketPlaces: Array<PublicKey>,
 	interval: number
 ) {
-	console.log('BOT IS RUNNING');
+	await runBot(connection, marketPlaces, interval);
 }
 
 start(connection, marketPlaces, 20_000);
